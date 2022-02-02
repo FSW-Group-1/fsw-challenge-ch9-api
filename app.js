113ccb9806767db6ed06f088b2a945ac0331f9fc
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-02-02 23:56:09 +0700

    Adding API documentaion

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// === Swagger UI ===
+  const swaggerJSON = require('./swagger.json');
+  const swaggerUI = require('swagger-ui-express');
+
+  app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON));
+// ==================
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
